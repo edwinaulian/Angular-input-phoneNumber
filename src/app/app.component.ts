a33d3d546113b6a76865ec3f20187491a72dd067
edwinauliano <edwinauliano@gmail.com>
2022-04-11 12:44:14 +0700

    [VALIDARION PHONE NUMBER]

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
-
+import { NgForm } from '@angular/forms';
+import { User } from './model/user';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+
+  mobNumberPattern = '^((\\+91-?)|0)?[0-9]{11}$';  
+  isValidFormSubmitted = false;
+  user = new User();
+
+  onFormSubmit(userForm: NgForm) {
+    this.isValidFormSubmitted = false;
+    if (userForm.invalid) { return }
+    this.isValidFormSubmitted = true;
+    userForm.resetForm();
+  }
 }
